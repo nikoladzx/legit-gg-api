60b7c62e5d0386387726487769c95e28132e1a64
nikoladzx <nikolastojkovic320@gmail.com>
2026-09-11 00:42:45 +0200

    added prisma

---
+++ src/database/prisma.service.ts
@@ -0,0 +1,31 @@
+import {
+  Injectable,
+  type OnModuleDestroy,
+  type OnModuleInit,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { getEnv, type Env } from '#/config/env.schema.js';
+import { PrismaClient } from '#/generated/prisma/client.js';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor(config: ConfigService<Env, true>) {
+    super({
+      adapter: new PrismaPg({
+        connectionString: getEnv(config).DATABASE_URL,
+      }),
+    });
+  }
+
+  async onModuleInit(): Promise<void> {
+    await this.$connect();
+  }
+
+  async onModuleDestroy(): Promise<void> {
+    await this.$disconnect();
+  }
+}
